21e48ac69439292d2a822f14b9bbc3f3888d6158
flyingbutton <thedrafttin@gmail.com>
2026-02-19 14:52:36 -0500

    Updated the default indentation setting for Umple files

--- plugin/umple.lua
+++ plugin/umple.lua
@@ -4,3 +4,14 @@ vim.filetype.add({
 		ump = "umple",
 	},
 })
+
+-- Default indentation for Umple files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "umple",
+	callback = function()
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+		vim.bo.softtabstop = 2
+		vim.bo.expandtab = true
+	end,
+})
